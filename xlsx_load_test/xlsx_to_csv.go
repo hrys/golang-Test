8aeaee1681446cf542e768d44c26df7b64564e82
Yoshio Hara <hrys1152@gmail.com>
2015-03-06 00:58:20 +0900

    xlsx_to_csvのファイル作成処理を変更。 コメント追記

--- xlsx_load_test/xlsx_to_csv.go
+++ xlsx_load_test/xlsx_to_csv.go
@@ -9,20 +9,26 @@ import (
 )
 
 func main() {
+
+	// xlsxファイル読み込み
 	file, err := xlsx.OpenFile("./xlsx/test.xlsx")
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 
-	csv_file, _ := os.OpenFile("./csv/test.csv", os.O_WRONLY|os.O_CREATE, 0600)
+	// 出力CSVファイル作成
+	csv_file, _ := os.Create("./csv/test.csv")
 	writer := csv.NewWriter(csv_file)
 	writer.Comma = ','
 
+	// シートの走査
 	for _, sheet := range file.Sheets {
+		// 行の走査
 		for _, row := range sheet.Rows {
 			var rowCells []string
 			rowCells = make([]string, 0)
+			// 行内のセル走査
 			for _, cell := range row.Cells {
 				rowCells = append(rowCells, cell.String())
 			}
